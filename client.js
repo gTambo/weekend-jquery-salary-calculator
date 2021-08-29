93b3069a916008defa4fdb4a25f4b76d6e95afdf
Greg Tambornino <gregtambornino@gmail.com>
2021-08-29 00:27:15 -0500

    stretch goal, tried lots of things, deleted salaries and rmv css from total cost

--- client.js
+++ client.js
@@ -29,18 +29,31 @@ function calculateCost(salArr) {
 
     if (totalMonthlyCost > maxMonthlyCost) {
         $('#total-monthly-cost').addClass('over-budget');
+    } else if (totalMonthlyCost <= maxMonthlyCost) {
+        $('#total-monthly-cost').removeClass('over-budget');
     }
+
 }
 
-function updateCost() {
-
+function updateCost(someNumber) {
+    let indexToDelete = someNumber - 1;
+    console.log('removing ', allSalaries[indexToDelete], 'from salaries');
+    allSalaries.splice(indexToDelete, 1, '0'); // remember to replace the removed value with '0', in case of deleting in random order
+    calculateCost(allSalaries);
 }
 
 function deleteEmployee() {
+    console.log('Employees: ', tableReference);
     let employeeRow = $(this).parent().parent();
     console.log($(this).parent().parent());
-    // updateCost($(this));
-    console.log(tableReference);
+    // let salaryToRemove =$('#employee-table-body').find("td.salary").text();
+    // console.log('found salary: ', salaryToRemove); // this returns a string of all salaries found, smashed together
+    let deleteButtonClass = employeeRow.attr('class');
+    console.log('delete button class: ', deleteButtonClass);
+    // updateCost(salaryToRemove)
+    updateCost(deleteButtonClass);
+    // let salaryToRemove = employeeRow.filter($('.salary')).text();
+    // console.log(salaryToRemove);
     employeeRow.remove();
 }
 
@@ -51,11 +64,14 @@ function addEmployeeInfo() {
     let idNumb = $('#id-number');
     let title = $('#job-title');
     let baseSalary = $('#annual-salary');
+    let employee = {};
 
     allSalaries.push(baseSalary.val());
     employeeTableNumber++;
     console.log(employeeTableNumber);
-    tableReference.push(employeeTableNumber);
+    employee.reference = employeeTableNumber;
+    employee.salary = baseSalary.val();
+    tableReference.push(employee);
 
     // console.log(firstName, lastName, idNumb, title, baseSalary);
     $('#employee-table-body').append(`
@@ -65,7 +81,7 @@ function addEmployeeInfo() {
             <td>${idNumb.val()}</td>
             <td>${title.val()}</td>
             <td class="salary">${baseSalary.val()}</td>
-            <td><button class="delete-button">Delete</button></td>
+            <td><button class="${employeeTableNumber} delete-button">Delete</button></td>
         </tr>
         `);
 
